f513f96177d7fc74cc415c0556b376c3ca6ccffc
Will James <wjames@thoughtworks.com>
2015-04-17 17:44:32 +1000

    restructured significantly to support more powerful selections on commits

--- test/basicConfig.js
+++ test/basicConfig.js
@@ -8,21 +8,20 @@ timelapse.clone('file://'+path.join(__dirname,'../font-dragr'))
 .then(function (repo) {
   console.log('Repository cloned');
 
-  repo.commits()
+  return repo.select()
     //.from(timelapse.commitMatcher('a9866e6084'))
     //.to(timelapse.commitMatcher('4991f9761c'))
     .beforeAll(function (checkout) {
-    console.log('This runs before all the commit actions.');
-  }).afterAll(function(checkout) {
-    console.log('This runs after all the commit actions.');
-  }).forEach(function(checkout) {
-    console.log('This runs for each commit.');
-  });
-
-  return repo.performActions();
+      console.log('This runs before all the commit actions.');
+    }).afterAll(function(checkout) {
+      console.log('This runs after all the commit actions.');
+    }).forEach(function(checkout) {
+      console.log('This runs for each commit.');
+    }).apply();
 })
 
 .then(function(repo){
+  console.log('cleaning up...');
   return repo.cleanup();
 })
 
