90920960d1e503fe0a89a2100de2b134c0542d55
Will James <wjames@thoughtworks.com>
2015-04-20 23:44:12 +1000

    fairly extensive changes... trying to move towards a simpler API.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,1 @@
 var expect = require('chai').expect;
-
-describe('history_screengrabber()', function () {
-    it('does something', function () {
-        expect(true).to.equal(false);
-    });
-});
