f146301d78fcd00b8a5b535db9cd0f101ba59571
Will James <wjames@thoughtworks.com>
2015-04-21 15:53:34 +1000

    cleaned up interface considerably

---
+++ examples/simpleConfig.js
@@ -0,0 +1,51 @@
+'use strict'
+
+var timelapse = require('../lib/timelapse');
+var path = require('path');
+
+var browserOptions = {
+  baseUrl: 'http://localhost:9000'
+};
+
+var repositoryUrl = 'file://'+path.join(__dirname,'../font-dragr');
+
+function installAndStartServer(ctx) {
+  ctx.exec('sed -i.bak "s/\\"grunt-bower-hooks\\": \\"~0.2.0\\"/\\"grunt-bower-requirejs\\": \\"~0.4.0\\"/" package.json');
+  ctx.exec('npm install');
+  ctx.exec('bower install --dev');
+  var child = ctx.spawn('grunt server');
+  ctx.wait(2000);
+  return child;
+}
+
+function teardownServer(ctx, child) {
+  ctx.kill(child);
+  ctx.exec('npm uninstall');
+}
+
+var browser = timelapse.browser(browserOptions);
+var repo = timelapse.clone(repositoryUrl);
+
+repo.select()
+  .from('a9866e6084')
+  .to('4991f9761c')
+  .forEach(function(commit) {
+    return repo.context(function(ctx) {
+      var child = installAndStartServer(ctx);
+
+      ctx.saveScreenshot(browser, commit);
+
+      teardownServer(ctx, child);
+
+      return ctx.whenDone();
+    });
+  }).apply()
+
+.then(function() {
+  repo.cleanup();
+  browser.shutdown();
+})
+
+.then(function() {
+  process.exit(0);
+});
--- test/test.js
+++ test/test.js
@@ -1,1 +1,69 @@
-var expect = require('chai').expect;
+var chai = require('chai')
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+var syncWrapper = require('../lib/syncWrapper');
+var promiseChain = require('../lib/promiseChain');
+
+describe('sychronous wrapper experiment', function() {
+  it('should make sure that functions are called in sequence', function() {
+    var testObj = {
+      count: 0,
+      quickFunc: function() {
+        var self = this;
+        return new Promise(function(resolve) {
+          resolve(++self.count);
+        });
+      },
+      slowFunc: function() {
+        var self = this;
+        return new Promise(function(resolve) {
+          setTimeout(function() {
+            resolve(++self.count);
+          }, 1000);
+        })
+      }
+    };
+
+    var wrappedObject = syncWrapper(testObj);
+
+    var one = wrappedObject.slowFunc();
+    var two = wrappedObject.quickFunc();
+
+    return Promise.all([
+      expect(one).to.eventually.equal(1),
+      expect(two).to.eventually.equal(2)]);
+  });
+
+  it('should provide a whenDone function to return a promise that resolves then the chain is done', function() {
+    var testObj = {
+      func: function() {
+        return new Promise(function(resolve) {
+          setTimeout(resolve, 1000);
+        });
+      }
+    };
+
+    var wrappedObject = syncWrapper(testObj);
+
+    wrappedObject.func();
+
+    return expect(wrappedObject.whenDone()).to.be.fulfilled;
+  });
+
+  it('should translate promise arguments to their resolutions to make the underlying call', function () {
+    var testObj = {
+      func: function(value) {
+        return ++value;
+      }
+    };
+
+    var wrappedObject = syncWrapper(testObj);
+
+    var testValue = 3;
+
+    var three = wrappedObject.func(Promise.resolve(testValue));
+
+    return expect(three).to.eventually.equal(testValue+1);
+  });
+})
